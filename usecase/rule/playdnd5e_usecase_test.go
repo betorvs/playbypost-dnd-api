a4ab6953bee21ac7f7a9029b9169c2c2b0c7a4e4
Roberto Scudeller <beto.rvs@gmail.com>
2021-08-30 23:58:41 +0200

    add tests for playdnd5e_usecase_test.go

--- usecase/rule/playdnd5e_usecase_test.go
+++ usecase/rule/playdnd5e_usecase_test.go
@@ -1,22 +1,89 @@
 package rule
 
-import "testing"
+import (
+	"strings"
+	"testing"
 
-func TestCalcProficiency(t *testing.T) {}
+	"github.com/stretchr/testify/assert"
+)
 
-func TestCalcAbilityModifier(t *testing.T) {}
+func TestCalcProficiency(t *testing.T) {
+	test1 := CalcProficiency(5)
+	exp1 := 3
+	assert.Equal(t, exp1, test1)
+}
 
-func TestCalcMaxHP(t *testing.T) {}
+func TestCalcAbilityModifier(t *testing.T) {
+	test1 := CalcAbilityModifier(8)
+	exp1 := -1
+	assert.Equal(t, exp1, test1)
+	test2 := CalcAbilityModifier(18)
+	exp2 := 4
+	assert.Equal(t, exp2, test2)
+}
 
-func TestXPNeeded(t *testing.T) {}
+func TestCalcMaxHP(t *testing.T) {
+	test1 := CalcMaxHP(1, 6, 2)
+	exp1 := 8
+	assert.Equal(t, exp1, test1)
+	test2 := CalcMaxHP(2, 6, 2)
+	exp2 := 13
+	assert.Equal(t, exp2, test2)
+}
 
-func TestCalculateSpellList(t *testing.T) {}
+func TestXPNeeded(t *testing.T) {
+	test1 := XPNeeded(0)
+	exp1 := 0
+	assert.Equal(t, exp1, test1)
+	test2 := XPNeeded(19)
+	exp2 := 355000
+	assert.Equal(t, exp2, test2)
+}
 
-func TestCalculateClassFeatureList(t *testing.T) {}
+func TestCalculateSpellList(t *testing.T) {
+	test1map, test1int := CalculateSpellList("wizard", 3)
+	assert.NotEmpty(t, test1map)
+	assert.GreaterOrEqual(t, test1int, 2)
+}
 
-func TestSpellSlotsMultiClass(t *testing.T) {}
+func TestCalculateClassFeatureList(t *testing.T) {
+	test1 := "ranger"
+	level1 := 1
+	chosen1 := []string{"forest", "giants"}
+	res1 := CalculateClassFeatureList(test1, level1, chosen1)
+	assert.GreaterOrEqual(t, len(res1), 2)
+	test2 := "ranger"
+	level2 := 2
+	chosen2 := []string{"dueling"}
+	res2 := CalculateClassFeatureList(test2, level2, chosen2)
+	assert.GreaterOrEqual(t, len(res2), 2)
+}
 
-func TestSpellsPerLevel(t *testing.T) {}
+func TestSpellSlotsMultiClass(t *testing.T) {
+	for _, v := range ClassWithSpell() {
+		res := SpellSlotsMultiClass(v, 4)
+		exp := 2
+		if strings.Contains(v, "-") || v == "warlock" {
+			exp = 0
+		}
+		assert.GreaterOrEqual(t, res, exp)
+	}
+}
+
+func TestSpellsPerLevel(t *testing.T) {
+	for _, v := range ClassWithSpell() {
+		sum := 0
+		if v == "warlock" {
+			continue
+		}
+		for i := 1; i < 21; i++ {
+			res := SpellsPerLevel(v, i)
+			assert.GreaterOrEqual(t, len(res), 1)
+			sum += i
+		}
+	}
+
+}
 
 func TestRaceStatistics(t *testing.T) {}
 
@@ -49,4 +116,3 @@ func TestCoinShortnameList(t *testing.T) {}
 func TestCoinShortName(t *testing.T) {}
 
 func TestExchangeRates(t *testing.T) {}
-
