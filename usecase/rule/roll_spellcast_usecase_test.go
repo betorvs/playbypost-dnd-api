a110e23ae0658b873d09443f3d384bfaaf927786
Roberto Scudeller <beto.rvs@gmail.com>
2021-09-10 10:28:29 +0200

    change tests on usecase/rule/roll_spellcast_usecase_test.go

--- usecase/rule/roll_spellcast_usecase_test.go
+++ usecase/rule/roll_spellcast_usecase_test.go
@@ -23,7 +23,7 @@ func TestCalcSpellcastAttackAndSave(t *testing.T) {
 	assert.NotEmpty(t, res1)
 	test2 := new(rule.SpellcastAbility)
 	test2.Ability = ability
-	test2.Level = 5
+	test2.Level = 4
 	test2.Class = "wizard"
 	test2.SpellName = "cure-wounds"
 	res2 := CalcSpellcastAttackAndSave(test2)
@@ -31,24 +31,29 @@ func TestCalcSpellcastAttackAndSave(t *testing.T) {
 	test2.SpellName = "fireball"
 	res3 := CalcSpellcastAttackAndSave(test2)
 	assert.NotEmpty(t, res3)
+	test2.Level = 5
 	test2.Monster = []string{"kobold"}
 	test2.MagicBonus = 1
 	res4 := CalcSpellcastAttackAndSave(test2)
 	assert.NotEmpty(t, res4)
+	test2.SpellLevel = 4
+	res5 := CalcSpellcastAttackAndSave(test2)
+	assert.NotEmpty(t, res5)
 	test2.SpellName = "chill-touch"
 	test2.Level = 15
 	test2.SpellLevel = 6
 	test2.Advantages = []string{"intelligence"}
 	test.DiceResult = 10
-	res5 := CalcSpellcastAttackAndSave(test2)
-	assert.NotEmpty(t, res5)
+	res6 := CalcSpellcastAttackAndSave(test2)
+	assert.NotEmpty(t, res6)
 	test3 := new(rule.SpellcastAbility)
 	test3.Ability = ability
 	test3.Level = 5
 	test3.Class = "cleric"
 	test3.SpellName = "cure-wounds"
-	res6 := CalcSpellcastAttackAndSave(test3)
-	assert.NotEmpty(t, res6)
+	res7 := CalcSpellcastAttackAndSave(test3)
+	assert.NotEmpty(t, res7)
+
 }
 
 func TestSpellForHealing(t *testing.T) {}
